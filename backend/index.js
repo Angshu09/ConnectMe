d1eabf645c3631593cebce84ef52ec74bad6f1f0
Angshu09 <angshudas191@gmail.com>
2026-08-03 08:25:40 +0530

    Modified

--- backend/index.js
+++ backend/index.js
@@ -3,19 +3,26 @@ import dotenv from "dotenv";
 import connectDb from "./config/db.js";
 import authRouter from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
-dotenv.config()
+dotenv.config();
+import cors from "cors";
+import userRouter from "./routes/user.routes.js";
 
 const PORT = process.env.PORT || 5000;
 
-const app = express()
+const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+app.use(express.json());
+app.use(cookieParser());
 
-app.use(express.json())
-app.use(cookieParser())
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
-app.use("/api/auth", authRouter)
-
-
-app.listen(PORT, ()=> {
-    connectDb()
-    console.log("server started")
-})
+app.listen(PORT, () => {
+  connectDb();
+  console.log("server started");
+});
